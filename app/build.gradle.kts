1735f8d42ea0a693479c699877416a68c2b60b75
p_dmweidu <p_dmweidu@tencent.com>
2025-02-12 10:24:39 +0800

    DOC：文档格式整理

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,4 @@ dependencies {
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation(project(mapOf("path" to ":lib")))
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("com.android.library") version "8.1.2" apply false
 
-    id("com.jfrog.bintray") version "1.8.4" apply false
     id("com.github.dcendents.android-maven") version "2.1" apply false
     //classpath 'com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.4'
     //classpath 'com.github.dcendents:android-maven-gradle-plugin:2.1'
